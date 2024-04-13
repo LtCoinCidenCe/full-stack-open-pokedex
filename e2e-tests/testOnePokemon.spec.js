c1dfc59ad7c19be4291e8809960166dd1c199ab9
Kizuna Akari <lecion20@outlook.com>
2024-04-13 20:57:42 +0800

    every test needs a goto or write a before each 11.9

---
+++ e2e-tests/testOnePokemon.spec.js
@@ -0,0 +1,19 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Test one pokemon', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('Butterfree can be navigated to', async ({ page }) => {
+    await page.getByText('butterfree').locator('..').click()
+    await expect(page.getByText('butterfree')).toBeVisible()
+    await expect(page.getByText('speed')).toBeVisible()
+    await expect(page.getByText('tinted lens')).toBeVisible()
+  })
+})
